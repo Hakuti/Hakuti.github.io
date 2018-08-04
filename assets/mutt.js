2318578cde91a5866518c0134717c94dda6dd5e1
Riseki <can1927@gmail.com>
2018-08-04 02:12:56 -0400

    Added Mutt Files

---
+++ assets/mutt.js
@@ -0,0 +1,50 @@
+$(document).ready(function() { 
+    
+    // var myElement = document.getElementById('myCarousel');
+    // var mc = new Hammer(myElement);
+    
+    // $("#myCarousel").on( "swipeleft", function( event ) 
+    // {
+    //   $(this).carousel('prev');
+    //   console.log("Here");
+    // } );
+
+    // $("#myCarousel").on( "swiperight", function( event ) 
+    // {
+    //   $(this).carousel('next');
+    //   console.log("Here")
+    // } );
+
+    //  $(".carousel-control-prev-icon").on( "click", function( event ) 
+    // {
+    //   $("#myCarousel").carousel('prev');
+    //   console.log("prev")
+    // } );
+
+    // $(".carousel-control-next-icon").on( "click", function( event ) 
+    // {
+    //   $("#myCarousel").carousel('next');
+    //   console.log("next")
+    // } );
+
+    
+    // $('#myCarousel').hammer().on('swipeleft', function(){
+    //       $(this).carousel('next'); 
+    //   })
+    //   $('#myCarousel').hammer().on('swiperight', function(){
+    //       $(this).carousel('prev'); 
+    //   })
+
+    // Hammer(myElement).on("swipeleft", function(){
+    //     $(this).carousel('next');
+    //     console.log("Swiped");
+    //     })
+
+    //var hammer = new Hammer();
+
+    // hammer($(myElement)).on('swiperight', function(){
+    //           $(this).carousel('next'); 
+    //     //   })
+    // });
+
+}); 
