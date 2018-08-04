425c6d6960210fdd827fb2dd12a5ba7e0d26a70e
Riseki <can1927@gmail.com>
2018-08-04 04:01:48 -0400

    Testing columns

--- assets/mutt.js
+++ assets/mutt.js
@@ -49,95 +49,95 @@ $(document).ready(function() {
 
 
 
-    var petUrl = "http://api.petfinder.com/pet.find";
-    var petApiKey = "aaf7ea34460505b8e7841f0512aae7a4"
-    $.ajax({
-        url: petUrl,
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
-            key: petApiKey,
-            animal: "dog",
-            "location": "32812",
-            output: "basic",
-            format: "json"
-        }
-    }).then(response=> {
-        var apiKey = "aaf7ea34460505b8e7841f0512aae7a4"
-        $.ajax({
-            url: petUrl,
-            jsonp: "callback",
-            dataType: "jsonp",
-            data: {
-                key: petApiKey,
-                animal: "dog",
-                "location": "32812",
-                output: "basic",
-                format: "json"
-            }
-        }).then(response=> {
-            console.log(response);
-            var dogName = response.petfinder.pets.pet[1].name.$t;
-            var dogAge = response.petfinder.pets.pet[1].age.$t;
-            var dogSex = response.petfinder.pets.pet[1].sex.$t;
-            var dogSize = response.petfinder.pets.pet[1].size.$t;
-            var dogDescription = response.petfinder.pets.pet[1].description.$t;
-            var largePic = response.petfinder.pets.pet[1].media.photos.photo[2].$t;
-            var photoGallery = response.petfinder.pets.pet[1].media.photos.photo;
-            // $("#dogModalPic").attr("src", largePic);
-            console.log(response.petfinder.pets.pet[1].media.photos.photo)
-            photoGallery.forEach(function(i){
-                // console.log(i);
-                if (i["@size"] == "x"){
-                    console.log(i.$t)
-                }
-            })
-            $("#dogModalPic").addClass("img-responsive");
-            $("#modalTitle").text(dogName);
-            $("#description").html(dogDescription);
-            $("#details").append($("<p>").addClass("col-md-4").text("Age: " + dogAge));
-            $("#details").append($("<p>").addClass("col-md-4").text("Size: " + dogSize));
-            $("#details").append($("<p>").addClass("col-md-4").text("Sex: " + dogSex));
-        });
-    });
+    // var petUrl = "http://api.petfinder.com/pet.find";
+    // var petApiKey = "aaf7ea34460505b8e7841f0512aae7a4"
+    // $.ajax({
+    //     url: petUrl,
+    //     jsonp: "callback",
+    //     dataType: "jsonp",
+    //     data: {
+    //         key: petApiKey,
+    //         animal: "dog",
+    //         "location": "32812",
+    //         output: "basic",
+    //         format: "json"
+    //     }
+    // }).then(response=> {
+    //     var apiKey = "aaf7ea34460505b8e7841f0512aae7a4"
+    //     $.ajax({
+    //         url: petUrl,
+    //         jsonp: "callback",
+    //         dataType: "jsonp",
+    //         data: {
+    //             key: petApiKey,
+    //             animal: "dog",
+    //             "location": "32812",
+    //             output: "basic",
+    //             format: "json"
+    //         }
+    //     }).then(response=> {
+    //         console.log(response);
+    //         var dogName = response.petfinder.pets.pet[1].name.$t;
+    //         var dogAge = response.petfinder.pets.pet[1].age.$t;
+    //         var dogSex = response.petfinder.pets.pet[1].sex.$t;
+    //         var dogSize = response.petfinder.pets.pet[1].size.$t;
+    //         var dogDescription = response.petfinder.pets.pet[1].description.$t;
+    //         var largePic = response.petfinder.pets.pet[1].media.photos.photo[2].$t;
+    //         var photoGallery = response.petfinder.pets.pet[1].media.photos.photo;
+    //         // $("#dogModalPic").attr("src", largePic);
+    //         console.log(response.petfinder.pets.pet[1].media.photos.photo)
+    //         photoGallery.forEach(function(i){
+    //             // console.log(i);
+    //             if (i["@size"] == "x"){
+    //                 console.log(i.$t)
+    //             }
+    //         })
+    //         $("#dogModalPic").addClass("img-responsive");
+    //         $("#modalTitle").text(dogName);
+    //         $("#description").html(dogDescription);
+    //         $("#details").append($("<p>").addClass("col-md-4").text("Age: " + dogAge));
+    //         $("#details").append($("<p>").addClass("col-md-4").text("Size: " + dogSize));
+    //         $("#details").append($("<p>").addClass("col-md-4").text("Sex: " + dogSex));
+    //     });
+    // });
     
-    var searchstring = "medium dog toys";
-    var ebayURL = "http://svcs.ebay.com/services/search/FindingService/v1";
-      ebayURL += "?OPERATION-NAME=findCompletedItems";
-      ebayURL += "&SERVICE-VERSION=1.13.0";
-      ebayURL += "&SERVICE-NAME=FindingService";
-      ebayURL += "&SECURITY-APPNAME=TimothyB-MuttMatc-PRD-8ed499e41-2cbab10d";
-      ebayURL += "&GLOBAL-ID=EBAY-US";
-      ebayURL += "&RESPONSE-DATA-FORMAT=JSON";
-      ebayURL += "&REST-PAYLOAD";
-      ebayURL += "&paginationInput.pageNumber=1";
-      ebayURL += "&paginationInput.entriesPerPage=10";
-      ebayURL += "&keywords=" + searchstring;
-      ebayURL += "&sortOrder=StartTimeNewest";
+    // var searchstring = "medium dog toys";
+    // var ebayURL = "http://svcs.ebay.com/services/search/FindingService/v1";
+    //   ebayURL += "?OPERATION-NAME=findCompletedItems";
+    //   ebayURL += "&SERVICE-VERSION=1.13.0";
+    //   ebayURL += "&SERVICE-NAME=FindingService";
+    //   ebayURL += "&SECURITY-APPNAME=TimothyB-MuttMatc-PRD-8ed499e41-2cbab10d";
+    //   ebayURL += "&GLOBAL-ID=EBAY-US";
+    //   ebayURL += "&RESPONSE-DATA-FORMAT=JSON";
+    //   ebayURL += "&REST-PAYLOAD";
+    //   ebayURL += "&paginationInput.pageNumber=1";
+    //   ebayURL += "&paginationInput.entriesPerPage=10";
+    //   ebayURL += "&keywords=" + searchstring;
+    //   ebayURL += "&sortOrder=StartTimeNewest";
     
-      $.ajax({
-        type: "GET",
-        url: ebayURL,
-        dataType: "jsonp",
-        // success: function(res){
-        //   console.log(res);
+    //   $.ajax({
+    //     type: "GET",
+    //     url: ebayURL,
+    //     dataType: "jsonp",
+    //     // success: function(res){
+    //     //   console.log(res);
           
-        // }
-      }).then(response => {
-          console.log(response)
-          for (i = 0; i < 3; i++){
-            ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[i].galleryURL[0];
-            ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[i].viewItemURL[0];
-            var productLink = $("<a>");
-            var productImg = $("<img>");
-            productLink.attr("href", ebayItemLink)
-            productImg.attr("src", ebayItemImg);
-            productLink.html(productImg);
+    //     // }
+    //   }).then(response => {
+    //       console.log(response)
+    //       for (i = 0; i < 3; i++){
+    //         ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[i].galleryURL[0];
+    //         ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[i].viewItemURL[0];
+    //         var productLink = $("<a>");
+    //         var productImg = $("<img>");
+    //         productLink.attr("href", ebayItemLink)
+    //         productImg.attr("src", ebayItemImg);
+    //         productLink.html(productImg);
     
-            $("#products").append(productLink)
-        }
-      });
-      ebayURL += "&sortOrder=StartTimeNewest";
+    //         $("#products").append(productLink)
+    //     }
+    //   });
+    //   ebayURL += "&sortOrder=StartTimeNewest";
         // var myElement = document.getElementById(‘myCarousel’);
         // var mc = new Hammer(myElement);
         
